033bee633b029251f5021c4e99df82fe65f6f0fb
Ean Jacob Gayban <ejacobg@outlook.com>
2023-07-28 10:46:35 -0700

    FeedController uses feed service

--- HackerNewsFeed/Controllers/FeedController.cs
+++ HackerNewsFeed/Controllers/FeedController.cs
@@ -1,13 +1,29 @@
+using HackerNewsFeed.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HackerNewsFeed.Controllers
 {
     public class FeedController : Controller
     {
+        private readonly IFeedService _feedService;
+
+        public FeedController(IFeedService feedService)
+        {
+            _feedService = feedService;
+        }
+        
         // GET
         public IActionResult Index()
         {
-            return View();
+            _feedService.Update();
+            return View(_feedService.Feed());
+        }
+        
+        // GET
+        public IActionResult Refresh()
+        {
+            _feedService.Clear();
+            return RedirectToAction("Index");
         }
     }
 }
