4291543e827c95963b78b8ef4149bf602f64c733
Ean Jacob Gayban <ejacobg@outlook.com>
2023-08-04 13:56:25 -0700

    Using SqlServerFeed

--- HackerNewsFeed/Startup.cs
+++ HackerNewsFeed/Startup.cs
@@ -39,6 +39,7 @@ namespace HackerNewsFeed
             
             // Add our custom service.
             services.AddSingleton<IFeedService, MemoryFeed>();
+            services.AddScoped<IFeedService, SqlServerFeed>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
